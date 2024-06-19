22357f870c7a994b51f2e3a2c3bbc03a30d9ed12
_Grimm <129088738+grimmTabi@users.noreply.github.com>
2024-06-19 00:08:54 -0300

    Add files via upload

---
+++ Camera Test 1/Scripts/Player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 500.0
+const FRICTION = 500.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if input.length() > 0:
+		velocity = input * MAX_SPEED
+		if input.x != 0: animated_sprite_2d.scale.x = sign(input.x)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	move_and_slide()
---
+++ Camera Test 1/Scripts/RoomTransiCam.gd
@@ -0,0 +1,36 @@
+extends Camera2D
+class_name RoomTransiCamera
+
+const HORIZONTAL_OFFSET : int = 56
+const VERTICAL_OFFSET : int = 64
+
+@onready var m_CameraHorizontalMovement : int = get_viewport_rect().size.x - HORIZONTAL_OFFSET
+@onready var m_CameraVerticalMovement : int = get_viewport_rect().size.y - VERTICAL_OFFSET
+# Node que a câmera vai seguir
+@export var TargetNode : Node2D  = null
+
+# Inicia a câmera atual aonde o player está apontando
+var m_CurrentRoom : Vector2 = Vector2.ZERO
+
+# Inicia o offset a partir do ponto de origem
+var m_OriginOffset : Vector2 = Vector2.ZERO
+
+func _ready():
+	m_OriginOffset = TargetNode.get_position()
+	set_position(m_OriginOffset)
+
+func _UpdateCameraPosition(direction : Vector2) -> void:
+	m_CurrentRoom += direction
+	set_position(m_OriginOffset + m_CurrentRoom * Vector2(m_CameraHorizontalMovement, m_CameraVerticalMovement))
+
+func _OnBodyEnteredCima(body):
+	_UpdateCameraPosition(Vector2.UP)
+
+func _OnBodyEnteredBaixo(body):
+	_UpdateCameraPosition(Vector2.DOWN)
+
+func _OnBodyEnteredEsqu(body):
+	_UpdateCameraPosition(Vector2.LEFT)
+
+func _OnBodyEnteredDire(body):
+	_UpdateCameraPosition(Vector2.RIGHT)
